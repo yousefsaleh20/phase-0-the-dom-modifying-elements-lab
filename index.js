4d1a596ed6b87836f21d8cab1d868a837a692a6c
Yousaef Saleh <yousefsaleh20@icloud.com>
2022-08-01 16:00:21 -0500

    commit tree 3f115199a6a8818e07ffdc77b0c071e336713daf

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const main = document.querySelector("#main");
+main.remove()
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+
+
+newHeader.textContent = "Yousef is the champion";
+document.body.append(newHeader);
